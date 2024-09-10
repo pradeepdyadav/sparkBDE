767db4dd289a33cee95841c7cd4fe765aedc040f
pradeep <pradeep.yadav1405@gmail.com>
2024-09-10 18:43:51 +0530

    fixed hive catalog configs

--- hive-metastore/entrypoint.sh
+++ hive-metastore/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export HADOOP_VERSION=3.3.4
 export HADOOP_HOME=/opt/hadoop-$HADOOP_VERSION
@@ -6,12 +6,18 @@ export HADOOP_CLASSPATH=$HADOOP_HOME/share/hadoop/tools/lib/aws-java-sdk-bundle-
 export HIVE_AUX_JARS_PATH=$HADOOP_CLASSPATH:${HADOOP_HOME}/share/hadoop/tools/lib/iceberg-hive-runtime-1.6.0.jar:${HADOOP_HOME}/share/hadoop/tools/lib/iceberg-aws-bundle-1.6.0.jar
 export JAVA_HOME=/usr/local/openjdk-8
 export DB_HOSTNAME=${DB_HOSTNAME:-localhost}
-
+export SKIP_SCHEMA_INIT=${SKIP_SCHEMA_INIT:-true}
 echo "Waiting for MySQL database on ${DB_HOSTNAME} to launch..."
 while ! nc -z $DB_HOSTNAME 3306; do
     sleep 1
 done
 
 echo "Starting Hive metastore service on $DB_HOSTNAME:3306"
-/opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType mysql
+
+if [[ "${SKIP_SCHEMA_INIT}" == "false" ]]; then
+  # handles schema initialization
+  echo "schema_initilization started..."
+  /opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType mysql
+fi
+
 /opt/apache-hive-metastore-3.0.0-bin/bin/start-metastore
